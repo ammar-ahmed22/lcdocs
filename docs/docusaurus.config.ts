20676db90a641ea3e7be2df8e6525489af712c1a
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-05-21 16:12:01 -0400

    ci: GitHub pages deployment workflow

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,16 +8,16 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ammar-ahmed22.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/lcdocs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ammar-ahmed22', // Usually your GitHub org/user name.
   projectName: 'lcdocs', // Usually your repo name.
-
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
